5e0d54bbc593f900bf8da1f37bc83181c0a8bf1d
MonicaIzzo <info@monicaizzo.net>
2023-05-26 09:12:02 +0200

    console.log('JS OK')

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('JS OK')
+
+
+
+
+
+
+/* Commento su più
+1. Chiesi all'utente il suo nome
+2. Poi chiedi il suo cognome
+3. poi chiedi il suo colore preferito
+4. infine scrivi sulla pagina: nomecognomecolorepreferito21   
+*/ 
+
